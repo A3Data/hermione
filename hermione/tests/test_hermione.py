fafdc57c6ebd770eae71150f13be7c17e5b30389
Neylson Crepalde <a3data03@localiza.com>
2020-05-08 22:26:54 -0300

    add test class

---
+++ hermione/tests/test_hermione.py
@@ -0,0 +1,13 @@
+from click.testing import CliRunner
+from hermione.cli import cli, logo
+
+def test_installation_ok():
+    runner = CliRunner()
+    res = runner.invoke(cli)
+    assert res.exit_code == 0
+
+def test_info():
+    runner = CliRunner()
+    res = runner.invoke(cli, ['info'])
+    assert logo in res.output
+
