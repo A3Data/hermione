c93c9794004fda20c2dca58b6f205e6750810572
Neylson Crepalde <a3data03@localiza.com>
2020-05-20 10:31:48 -0300

    insert train command test

--- hermione/tests/test_hermione.py
+++ hermione/tests/test_hermione.py
@@ -7,6 +7,11 @@ def test_installation_ok():
     res = runner.invoke(cli)
     assert res.exit_code == 0
 
+def test_train_command_ok():
+    runner = CliRunner()
+    res = runner.invoke(cli, ['train'])
+    assert res.exit_code == 0
+
 def test_info():
     runner = CliRunner()
     res = runner.invoke(cli, ['info'])
