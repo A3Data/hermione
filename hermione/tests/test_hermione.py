b272ba1e9d152200993bc6211ca745674f87cb7d
Rodrigo Torres <ratorres7@gmail.com>
2021-04-02 12:24:49 -0300

    Fixing typo

--- hermione/tests/test_hermione.py
+++ hermione/tests/test_hermione.py
@@ -18,5 +18,5 @@ def test_info():
     assert logo in res.output
 
 def test_implementation_script_folders():
-    assert os.path.exists(os.path.join(os.getcwd(), 'hermione', module_templates, '__IMPLEMENTED_BASE__'))
-    assert os.path.exists(os.path.join(os.getcwd(), 'hermione', module_templates, '__NOT_IMPLEMENTED_BASE__'))
+    assert os.path.exists(os.path.join(os.getcwd(), 'hermione', 'module_templates', '__IMPLEMENTED_BASE__'))
+    assert os.path.exists(os.path.join(os.getcwd(), 'hermione', 'module_templates', '__NOT_IMPLEMENTED_BASE__'))
